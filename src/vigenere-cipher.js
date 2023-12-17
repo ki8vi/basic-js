91ce54a4764e92f19d6b475d44155fb3620e7cf8
ki8vi <oneofbln@gmail.com>
2023-12-17 15:05:17 +0600

    feat: solve Vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,78 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isNotReverse = true) {
+    this.isNotReverse = isNotReverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabetic = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  encrypt(sentence, key) {
+    if(!(sentence && key)) {
+      throw new Error("Incorrect arguments!")
+    }
+    sentence = sentence.toUpperCase();
+    key = key.toUpperCase();
+    const expandedKeyAndSentence = this.expandKey(key, sentence);
+    let finalStr = []
+    for(let i = 0; i < expandedKeyAndSentence.sentence.length; i++) {
+      if(/[A-Z]/.test(expandedKeyAndSentence.sentence[i])) {
+        finalStr.push(this.alphabetic[(this.alphabetic.indexOf(expandedKeyAndSentence.sentence[i]) + this.alphabetic.indexOf(expandedKeyAndSentence.key[i])) % 26])
+      } 
+    }
+    for(let i = 0; i < sentence.length; i++) {
+      if(/[^A-Z]/.test(sentence[i])) {
+        finalStr.splice(i, 0, sentence[i])
+      }
+    }
+    if(!this.isNotReverse) {
+      finalStr.reverse()
+    }
+    return finalStr.join("")
+  }
+ 
+  decrypt(sentence, key) {
+    if(!(sentence && key)) {
+      throw new Error("Incorrect arguments!")
+    }
+    sentence = sentence.toUpperCase();
+    key = key.toUpperCase();
+    const expandedKeyAndSentence = this.expandKey(key, sentence);
+    
+    let finalStr = []
+    for(let i = 0; i < expandedKeyAndSentence.sentence.length; i++) {
+      if(/[A-Z]/.test(expandedKeyAndSentence.sentence[i])) {
+        if((this.alphabetic.indexOf(expandedKeyAndSentence.sentence[i]) - this.alphabetic.indexOf(expandedKeyAndSentence.key[i])) % 26 < 0) {
+          finalStr.push(this.alphabetic[((this.alphabetic.indexOf(expandedKeyAndSentence.sentence[i]) - this.alphabetic.indexOf(expandedKeyAndSentence.key[i])) + 26) % 26])
+        } else {
+          finalStr.push(this.alphabetic[(this.alphabetic.indexOf(expandedKeyAndSentence.sentence[i]) - this.alphabetic.indexOf(expandedKeyAndSentence.key[i])) % 26])
+        }
+        
+      } 
+    }
+    for(let i = 0; i < sentence.length; i++) {
+      if(/[^A-Z]/.test(sentence[i])) {
+        finalStr.splice(i, 0, sentence[i])
+      }
+    }
+    if(!this.isNotReverse) {
+      finalStr.reverse()
+    }
+    return finalStr.join("")
+  }
+  expandKey(key, sentence) {
+    const clearedSentence = sentence.replace(/[^A-Z]/g, '');
+    let expandedKey = "";
+    let index = 0;
+    while(expandedKey.length !== clearedSentence.length) {
+      expandedKey += key[index];
+      index++
+      if(index >= key.length) {
+        index = 0
+      }
+    }
+    return {
+      sentence: clearedSentence,
+      key: expandedKey
+    };
   }
 }
 
